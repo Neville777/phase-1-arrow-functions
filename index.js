277427f738f3db8f2a41b4fcf1fa9fb73ed550ba
neville777 <neville.james@student.moringaschool.com>
2023-09-19 18:03:42 +0300

    arrow-functions

---
+++ index.js
@@ -0,0 +1,12 @@
+// Define the divide function
+function divide(num1 = 2000, num2 = 100) {
+    return num1 / num2;
+}
+
+// Has an arrow function called square
+
+const square = (num3) => num3 * num3;
+
+// Has arrow function called add
+
+const add = (num3,num4) => num3 + num4;
